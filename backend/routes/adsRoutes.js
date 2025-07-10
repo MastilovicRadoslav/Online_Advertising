a64f75f8b7a7817149e5e851410372347a83103e
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-10 17:35:44 +0200

    small mistake corrected

--- backend/routes/adsRoutes.js
+++ backend/routes/adsRoutes.js
@@ -9,7 +9,7 @@ router.post("/", authMiddleware, createAd);
 router.get("/", authMiddleware, getAds);
 router.get("/:id", authMiddleware, getAdById);
 router.put("/:id", authMiddleware, updateAd);
-router.delete("/id", authMiddleware, deleteAd);
+router.delete("/:id", authMiddleware, deleteAd);
 
 
 
