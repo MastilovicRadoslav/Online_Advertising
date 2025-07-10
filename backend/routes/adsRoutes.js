17cc2063957a362257b4d1936f2f9ff4cb317e35
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-10 14:37:29 +0200

    adsRoutes new routes

--- backend/routes/adsRoutes.js
+++ backend/routes/adsRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createAd } = require("../controllers/adsController");
+const { createAd, getAds, getAdById, updateAd, deleteAd } = require("../controllers/adsController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Zaštićena ruta
 router.post("/", authMiddleware, createAd);
+router.get("/", authMiddleware, getAds);
+router.get("/:id", authMiddleware, getAdById);
+router.put("/:id", authMiddleware, updateAd);
+router.delete("/id", authMiddleware, deleteAd);
+
+
+
 
 module.exports = router;
