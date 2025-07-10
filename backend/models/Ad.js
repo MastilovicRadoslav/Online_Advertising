9ae655248f475539224ee134c2a492529ae7ebec
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-10 11:33:48 +0200

    Ad

---
+++ backend/models/Ad.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const adSchema = new mongoose.Schema({
+    title: {type: String, required: true },
+    description: { type: String },
+    imageUrl: { type: String },
+    price: { type: String },
+    category: {
+        type: String,
+        enum: [ 
+            "clothing", "tools", "sports", "accessories", "furniture",
+            "pets", "games", "books", "technology"
+        ],
+        required: true,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    city: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Ad", adSchema);
