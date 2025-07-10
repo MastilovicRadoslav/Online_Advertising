5b85b5ed7f8c296368e5f18f4529c5b847147760
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-10 22:10:59 +0200

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import SingupPage from './pages/SignupPage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<LoginPage />}/>
       <Route path='/signup' element={<SingupPage/>}/>
+      <Route path='/' element={<HomePage/>}/>
     </Routes>
   )
 }
