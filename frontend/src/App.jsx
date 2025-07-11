e61249aeafbda9547e70855f08341de3fb2c9fe3
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-11 21:39:41 +0200

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import AdDetailsPage from "./pages/AdDetailsPage.jsx";
 import CreateEditAdPage from "./pages/CreateEditAdPage.jsx";
 import Navbar from "./components/Navbar.jsx";
+import { Layout } from "antd";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/ads/:id" element={<AdDetailsPage />} />
-        <Route path="/create" element={<CreateEditAdPage />} />
-        <Route path="/edit/:id" element={<CreateEditAdPage />} /> 
-      </Routes>
+      <Layout style={{ marginTop: 64 }}>
+        <div style={{ maxWidth: 1200, margin: "0 auto", padding: "20px" }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/ads/:id" element={<AdDetailsPage />} />
+            <Route path="/create" element={<CreateEditAdPage />} />
+            <Route path="/edit/:id" element={<CreateEditAdPage />} />
+          </Routes>
+        </div>
+      </Layout>
     </>
   );
 };
